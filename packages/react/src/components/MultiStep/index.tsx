601e2160ca75b057324e3df5b8c823ad84397e6a
Michel Domingos <micheldslive@gmail.com>
2023-10-12 09:52:23 -0300

    feat: adding multistep component / storie

---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,27 @@
+import { Label, MultiStepContainer, Step, Steps } from "./styles";
+
+export interface MultiStepProps {
+  size: number;
+  currentStep?: number;
+  label?: string;
+}
+
+export const MultiStep = ({ size, currentStep = 1, label }: MultiStepProps) => {
+  return (
+    <MultiStepContainer>
+      {label ? <Label>{label}</Label> : null}
+
+      <Steps
+        css={{
+          "--steps-size": size,
+        }}
+      >
+        {Array.from({ length: size }, (_, i) => i + 1).map((step) => (
+          <Step key={step} active={currentStep >= step} />
+        ))}
+      </Steps>
+    </MultiStepContainer>
+  );
+};
+
+MultiStep.displayName = "MultiStep";
