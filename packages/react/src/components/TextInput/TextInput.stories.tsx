ba76b0133166246b51db8c8b9a5870d48e340bd6
Michel Domingos <micheldslive@gmail.com>
2023-10-12 09:53:36 -0300

    feat: adding textinput component / storie

---
+++ packages/react/src/components/TextInput/TextInput.stories.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/jsx-key */
+import { TextInput, type TextInputRootProps } from "@micode-ui/react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { Envelope, Eye, EyeSlash } from "phosphor-react";
+
+export default {
+  title: "Form/TextInput",
+  component: TextInput.Root,
+  args: {
+    children: <TextInput.Input placeholder="Placeholder" />,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta;
+
+export const Default: StoryObj<TextInputRootProps> = {};
+
+export const WithIcon: StoryObj<TextInputRootProps> = {
+  args: {
+    children: [
+      <TextInput.LeftIcon>
+        <Envelope weight="bold" />
+      </TextInput.LeftIcon>,
+      <TextInput.Input placeholder="Placeholder" />,
+    ],
+  },
+};
+
+export const WithPrefix: StoryObj<TextInputRootProps> = {
+  args: {
+    children: [
+      <TextInput.Prefix>call.co/</TextInput.Prefix>,
+      <TextInput.Input placeholder="Placeholder" />,
+    ],
+  },
+};
+
+export const WithError: StoryObj<TextInputRootProps> = {
+  args: {
+    error: true,
+  },
+};
+
+export const Disabled: StoryObj<TextInputRootProps> = {
+  args: {
+    disabled: true,
+    children: <TextInput.Input placeholder="Placeholder" />,
+  },
+};
+
+export const Password: StoryObj<TextInputRootProps> = {
+  args: {
+    children: [
+      <TextInput.LeftIcon>
+        <Envelope weight="bold" />
+      </TextInput.LeftIcon>,
+      <TextInput.Input placeholder="Type your password..." type="password" />,
+      <TextInput.RightIcon
+        activeIcon={<Eye weight="bold" />}
+        inactiveIcon={<EyeSlash weight="bold" />}
+      />,
+    ],
+  },
+};
---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,153 @@
+import {
+  createContext,
+  forwardRef,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import type {
+  ComponentProps,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+} from "react";
+import {
+  StyledInput,
+  StyledLeftIcon,
+  StyledPrefix,
+  StyledRightIcon,
+  TextInputContainer,
+} from "./styles";
+
+interface TextInputContextType {
+  isTextVisible: boolean;
+  toggleVisibility: () => void;
+  setTextVisibility: Dispatch<SetStateAction<boolean>>;
+  disabled: boolean;
+}
+
+const TextInputContext = createContext({} as TextInputContextType);
+
+/* ----------------------------------------------------------------------------------------------- */
+
+interface TextInputRootProps {
+  children?: ReactNode;
+  error?: boolean;
+  disabled?: boolean;
+}
+
+const TextInputRoot = ({
+  children,
+  error,
+  disabled = false,
+}: TextInputRootProps) => {
+  const [isTextVisible, setTextVisibility] = useState(true);
+
+  const toggleVisibility = useCallback(() => {
+    setTextVisibility((state) => !state);
+  }, []);
+
+  // const toggleVisibilityIcon = isTextVisible ? (
+  //   <EyeSlash weight='bold' />
+  // ) : (
+  //   <Eye weight='bold' />
+  // );
+
+  return (
+    <TextInputContext.Provider
+      value={{ isTextVisible, toggleVisibility, setTextVisibility, disabled }}
+    >
+      <TextInputContainer error={error}>{children}</TextInputContainer>
+    </TextInputContext.Provider>
+  );
+};
+
+TextInputRoot.displayName = "TextInput.Root";
+
+/* ----------------------------------------------------------------------------------------------- */
+
+interface TextInputLeftIconProps {
+  children?: ReactNode;
+}
+
+const TextInputLeftIcon = ({ children }: TextInputLeftIconProps) => {
+  return <StyledLeftIcon>{children}</StyledLeftIcon>;
+};
+
+TextInputLeftIcon.displayName = "TextInput.LeftIcon";
+
+/* ----------------------------------------------------------------------------------------------- */
+
+interface TextInputPrefixProps {
+  children?: string;
+}
+
+const TextInputPrefix = ({ children }: TextInputPrefixProps) => {
+  return <StyledPrefix>{children}</StyledPrefix>;
+};
+
+TextInputPrefix.displayName = "TextInput.Prefix";
+
+/* ----------------------------------------------------------------------------------------------- */
+
+type TextInputProps = ComponentProps<typeof StyledInput>;
+
+const TextInputInput = forwardRef<HTMLInputElement, TextInputProps>(
+  ({ type, ...props }, ref) => {
+    const { isTextVisible, setTextVisibility, disabled } =
+      useContext(TextInputContext);
+
+    useEffect(() => {
+      if (type === "password") {
+        setTextVisibility(false);
+      }
+    }, [type, setTextVisibility]);
+
+    const inputType = isTextVisible ? "text" : "password";
+
+    return (
+      <StyledInput type={inputType} disabled={disabled} ref={ref} {...props} />
+    );
+  }
+);
+
+TextInputInput.displayName = "TextInput.Input";
+
+/* ----------------------------------------------------------------------------------------------- */
+
+interface TextInputRightIconProps {
+  activeIcon?: ReactNode;
+  inactiveIcon?: ReactNode;
+}
+
+const TextInputRightIcon = ({
+  activeIcon,
+  inactiveIcon,
+}: TextInputRightIconProps) => {
+  const { isTextVisible, toggleVisibility } = useContext(TextInputContext);
+
+  // This makes no sense, but when the text is visible, the inactiveIcon is shown to hide the text
+  const icon = isTextVisible ? inactiveIcon : activeIcon;
+
+  return <StyledRightIcon onClick={toggleVisibility}>{icon}</StyledRightIcon>;
+};
+
+TextInputRightIcon.displayName = "TextInput.RightIcon";
+
+/* ----------------------------------------------------------------------------------------------- */
+
+export const TextInput = {
+  Root: TextInputRoot,
+  LeftIcon: TextInputLeftIcon,
+  Prefix: TextInputPrefix,
+  Input: TextInputInput,
+  RightIcon: TextInputRightIcon,
+};
+export type {
+  TextInputRootProps,
+  TextInputLeftIconProps,
+  TextInputPrefixProps,
+  TextInputProps,
+  TextInputRightIconProps,
+};
