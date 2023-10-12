cc2a2d368ae7a054772ee252eebd167748537e32
Michel Domingos <micheldslive@gmail.com>
2023-10-12 09:52:39 -0300

    feat: adding popover component / storie

---
+++ packages/react/src/components/Popover/Popover.stories.tsx
@@ -0,0 +1,52 @@
+import { Heading, Popover, type PopoverProps, Text } from "@micode-ui/react";
+import type { Meta } from "@storybook/react";
+import { Info } from "phosphor-react";
+
+import { IconButton } from "../IconButton";
+
+export default {
+  title: "Feedback/Popover",
+  component: Popover,
+} as Meta<PopoverProps>;
+
+export const Default = () => (
+  <Popover.Root>
+    <Popover.Trigger asChild>
+      <IconButton>
+        <Info />
+      </IconButton>
+    </Popover.Trigger>
+    <Popover.Content side="bottom">
+      <Heading size="md">Popover</Heading>
+      <Text size="sm">This is a Popover component</Text>
+    </Popover.Content>
+  </Popover.Root>
+);
+
+export const WithoutArrow = () => (
+  <Popover.Root>
+    <Popover.Trigger asChild>
+      <IconButton>
+        <Info />
+      </IconButton>
+    </Popover.Trigger>
+    <Popover.Content side="bottom" hideArrow>
+      <Heading size="md">Popover</Heading>
+      <Text size="sm">This is a Popover component</Text>
+    </Popover.Content>
+  </Popover.Root>
+);
+
+export const DefaultOpen = () => (
+  <Popover.Root defaultOpen>
+    <Popover.Trigger asChild>
+      <IconButton>
+        <Info />
+      </IconButton>
+    </Popover.Trigger>
+    <Popover.Content side="bottom">
+      <Heading size="md">Popover</Heading>
+      <Text size="sm">This is a Popover component</Text>
+    </Popover.Content>
+  </Popover.Root>
+);
